8ff1d24663e1fec9a62f87a8cb274b878292ac55
sandersa <aric.sanders@nist.gov>
2025-04-07 14:38:27 -0600

    fix init issues

--- asmg_workflow/__init__.py
+++ asmg_workflow/__init__.py
@@ -57,9 +57,9 @@ for module in sorted(DE_API_MODULES.keys()):
                 time_difference = end_timer - start_timer
                 print(("It took {0} s to import {1}".format(time_difference.total_seconds(), module)))
                 start_timer = end_timer
-    except:
+    except Exception as e:
         print(f"The {module} failed to import")
-        pass
+        print(e)
 if TIMED_IMPORT:
     end_timer = datetime.datetime.utcnow()
     time_difference = end_timer - first_timer
